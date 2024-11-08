1b0a606d614745cd43dcb3f901bf2a5640b15a90
VSOP40 <phongnguyenwm3@gmail.com>
2024-11-08 16:02:09 +0700

    video #52 design homepage

--- src/components/Home/HomePage.js
+++ src/components/Home/HomePage.js
@@ -1,7 +1,14 @@
 import React from "react";
+import videoHomePage from "../../assets/video-homepage.mp4";
 
 const HomePage = (props) => {
-  return <div className="homepage-container">Home page component</div>;
+  return (
+    <div className="homepage-container">
+      <video autoPlay loop muted>
+        <source src={videoHomePage} type="video/mp4" />
+      </video>
+    </div>
+  );
 };
 
 export default HomePage;
