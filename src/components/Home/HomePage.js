0ce987d732b56a97d733f314caa2d935d0b448b8
VSOP40 <phongnguyenwm3@gmail.com>
2024-11-08 17:18:27 +0700

    video #53.1 design new header

--- src/components/Home/HomePage.js
+++ src/components/Home/HomePage.js
@@ -7,6 +7,17 @@ const HomePage = (props) => {
       <video autoPlay loop muted>
         <source src={videoHomePage} type="video/mp4" />
       </video>
+      <div className="homepage-content">
+        <div className="homepage-title">There's is a better way to ask</div>
+        <div className="homepage-desc">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error rerum
+          tenetur quis accusamus quo, impedit qui quisquam aperiam molestiae
+          nesciunt?
+        </div>
+        <div>
+          <button className="homepage-btn">Get started. It's free</button>
+        </div>
+      </div>
     </div>
   );
 };
