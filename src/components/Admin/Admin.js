7030f2248eb0ddf0a58fceeb232639ea5aaa131f
VSOP40 <phongnguyenwm3@gmail.com>
2024-11-09 11:43:24 +0700

    video #54 take prosidebar form git hub & design side bar for admin page

--- src/components/Admin/Admin.js
+++ src/components/Admin/Admin.js
@@ -1,7 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
+import SideBar from "./SideBar";
+import "./Admin.scss";
+import { FaBars } from "react-icons/fa";
 
 const Admin = (props) => {
-  return <div>Admin component</div>;
+  const [collapsed, setCollapsed] = useState(false);
+  return (
+    <div className="admin-container">
+      <div className="admin-sidebar">
+        <SideBar collapsed={collapsed} />
+      </div>
+      <div className="admin-content">
+        <FaBars onClick={() => setCollapsed(!collapsed)} />
+        admin content
+      </div>
+    </div>
+  );
 };
 
 export default Admin;
